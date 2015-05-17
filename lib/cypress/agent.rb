c1a5330737d77b3b02e7dfa2e9f503d0b797317b
Thomas Holmes <thomas@holmes.io>
2015-05-17 15:27:20 -0400

    Add some exploratory dispatcher and transaction stuff.

--- lib/cypress/agent.rb
+++ lib/cypress/agent.rb
@@ -4,6 +4,12 @@ module Cypress
   class Agent
     def initialize
       @socket = SocketIO::Client::Simple::Client.new(Cypress.configuration.url)
+      @dispatcher = Cypress::Dispatcher.new
+    end
+
+    def dispatch(message)
+      puts "dispatching"
+      @dispatcher.dispatch(message)
     end
 
     def reconnect
@@ -13,6 +19,7 @@ module Cypress
     def start!
       # Doesn't close over ivars, must be instance_eval/execing
       socket = @socket
+      agent = self
 
       @socket.on :connect do
         begin
@@ -27,17 +34,75 @@ module Cypress
 
       @socket.on :"remote:request" do |id, message, args|
         puts "id:#{id} | msg: #{message} | args: #{args}"
-        begin
-          response = Cypress.world.execute_hook(message.to_sym, args) ||
-            { error: "No handler registered for #{message}" }
-        rescue => e
-          response = { error: e.message }
-        end
+        msg = { id: id, message: message, args: args }
+
+        response = agent.dispatch(msg)
+
         socket.emit('remote:response', id, response)
       end
 
       @socket.on :disconnect do
         puts "socket disconnected"
+      end
+    end
+  end
+end
+
+module Cypress
+  class Dispatcher
+    def initialize
+      @strategy = Cypress::Strategy::Transaction.new
+    end
+
+    def dispatch(message)
+      puts "in dispatch #{message}"
+      msg = message[:message]
+      args = message[:args]
+
+      response = {}
+
+      case msg
+      when :before, :before_each, :after, :after_each
+        @strategy.in
+      else
+        puts "running user hook #{msg}"
+        response = run_user_hook(msg, args)
+      end
+
+      response
+    end
+
+    def run_user_hook(name, args)
+      begin
+        Cypress.world.execute_hook(name.to_sym, args) ||
+          { __error: "No handler registered for #{name}" }
+      rescue => e
+        { __error: e.message, __stack: e.backtrace.join("\n") }
+      end
+    end
+  end
+end
+
+module Cypress
+  module Strategy
+    # HACK!
+    class Transaction
+      def in
+        ActiveRecord::Base.connection.execute('BEGIN')
+      end
+
+      def out
+        ActiveRecord::Base.connection.execute('ROLLBACK')
+      end
+    end
+
+    class DbCleaner
+      def in
+        ::DatabaseCleaner.start
+      end
+
+      def out
+        ::DatabaseCleaner.clean
       end
     end
   end
